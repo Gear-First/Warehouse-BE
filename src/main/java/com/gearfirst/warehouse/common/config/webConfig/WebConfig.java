d4e02eb5087fda84c08e6ab65a5c7357f2955794
qwice <wjdtkdrl15432@gmail.com>
2025-10-29 11:33:31 +0900

    fix: webconfig add patch

--- src/main/java/com/gearfirst/warehouse/common/config/webConfig/WebConfig.java
+++ src/main/java/com/gearfirst/warehouse/common/config/webConfig/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL에 대해
                 .allowedOrigins("http://localhost:5173") // 허용할 origin, 여러 개도 가능
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")   // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 인증 허용
                 .maxAge(3600);        // 프리플라이트 응답 캐시 시간(초)
