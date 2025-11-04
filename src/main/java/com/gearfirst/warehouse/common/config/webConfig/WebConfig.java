3f06aa3b239bcff037d0f7bc2a5e53f6e2d65614
Hyun Heechan <hyeonhc328@gmail.com>
2025-11-04 13:06:40 +0900

    fix(config): WebConfig가 스프링 컨테이너에 등록되지 않도록 변경

--- src/main/java/com/gearfirst/warehouse/common/config/webConfig/WebConfig.java
+++ src/main/java/com/gearfirst/warehouse/common/config/webConfig/WebConfig.java
@@ -1,10 +1,9 @@
 package com.gearfirst.warehouse.common.config.webConfig;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
